259e5e6abc7f7bba001e03ab606819c6ca659dca
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2023-12-16 22:33:20 +0600

    create basic server blueprint

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("object");
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5022;
+const cors = require("cors");
+
+
+//<<<<<---------------------------------------- middlewares ----------------------------------------->>>>>
+app.use(express.json());
+app.use(cors());
+//<<<<<---------------------------------------- middlewares ----------------------------------------->>>>>
+
+
+app.get('/',(req,res)=>{
+    res.send("Server is RuNNinG......!!!");
+})
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+})
